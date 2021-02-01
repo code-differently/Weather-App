10da5efe33deb1a628af06612360acf3b36aecc7
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-01-31 19:16:48 -0500

    added function

--- weather.js
+++ weather.js
@@ -1,14 +1,17 @@
 
 
-// DOM Elements
+/* DOM Elements */
 const cityState = document.querySelector(".cityState");
 const temperature = document.querySelector(".temperature");
 
-//Ask user zip code
-// const userZip = prompt(`What's your Zip Code?`);
-// const userLink = '' + parseInt(userZip);  
 
+/* Helper Functions */
 
+function convertTemp(temp) {
+  const celsius = temp -273;
+  let fahrenheit = Math.floor(celsius * (9/5) + 32);
+  return fahrenheit;
+}
 
 /*  Fetch Functions */
 
@@ -18,12 +21,15 @@ const temperature = document.querySelector(".temperature");
           const {latitude, longitude} = position.coords; 
           const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=68824d84dff94b6a8921c156b462a3d1`)
           const data = await response.json();
-          const city = data.results[0].components.city;
-          const state = data.results[0].components.state_code;
-          console.log(`${city}, ${state}`);
+          
+          console.log(results);
 
-          cityState.textContent = `$[city], ${state}`;
-          currentTemp(city, state);
+          const city1 = data.results[0].components.city;
+          const stateData = data.results[0].components.state_code;
+
+          cityState.textContent = `${city1}, ${stateData}`;
+
+          currentTemp(city1, stateData);
         }
         catch(error) {
           console.log(error);
@@ -31,15 +37,16 @@ const temperature = document.querySelector(".temperature");
 }
 
 
-navigator.geolocation.getCurrentPosition(successfulLookup, console.log);
+
 
 // Fetch weather at openweathermap api
     const currentTemp = async (city, state) => {
       try {
         const temp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=0312d87f192d54ce3485295a535d403f`)
         const data = await temp.json();
-        let currTemp = Math.trunc(data.main.temp/10);
-        //currTemp = 29;
+
+        let currTemp = convertTemp(data.main.temp);
+
         temperature.innerHTML = `${currTemp}&#x2109`;
 
       }
@@ -48,3 +55,4 @@ navigator.geolocation.getCurrentPosition(successfulLookup, console.log);
       }
     }
 
+    navigator.geolocation.getCurrentPosition(successfulLookup, console.log);

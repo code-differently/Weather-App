ed62462580710cf1c2fea97806c0218d2b07dcc1
Andre Logan <andrelogan@andres-air.myfiosgateway.com>
2021-02-04 22:49:44 -0500

    weather app almost done

--- weatherapp.js
+++ weatherapp.js
@@ -1,59 +1,54 @@
 
-
+const apiKey = "9d60ac7e8eb3d17560a9e4046dc3f540";
 // add click handler for button
 document.getElementById("forecast").addEventListener("click", getData);
 
 function getData() {
-    const weatherInfo = document.getElementById("weatherInfo");
+    // console.log("get data");
+    // const weatherInfo = document.getElementById("weatherInfo");
     // need the zipcode entered
     let zipcode =
     document.getElementById("zipcode").value;
+    // console.log("zipcode is " + zipcode);
     // set up api
     const endpoint =
-    "http://api.zippopotam.us/US/"+zipcode;
-    const query = "?zip=" + zipcode + "&units=imperial&appid=";
+    "https://api.openweathermap.org/data/2.5/forecast"
+    // "https://api.openweathermap.org/data/2.5/weather";
+    // "http://api.zippopotam.us/US/"+zipcode;
+    const query = "?zip=" + zipcode + "&units=imperial&appid=" + apiKey;
     const url = endpoint + query;
+    // console.log(url);
     const xhr = new XMLHttpRequest();
     // set up response
     xhr.addEventListener("load", responseReceivedHandler);
     // required for json
     xhr.responseType = "json";
-    // open the connection
-    xhr.open("GET", endpoint);
+    // open the connection "endpoint"
+    xhr.open("GET", url);
     xhr.send();
   
 };
 
-// const endpoint =
-// "http://www.7timer.info/bin/api.pl?product=civillight&lon=${lon}&lat=${lat}&output=json";
-// const query = "?zip=" + zipcode + "&units=imperial&appid=";
-// const url = endpoint + query;
-// const xhr2 = new XMLHttpRequest();
-// // set up response
-// xhr2.addEventListener("load", responseReceivedHandler);
-// // required for json
-// xhr2.responseType = "json";
-// // open the connection
-// xhr2.open("GET", endpoint);
-// xhr2.send();
-
-
 function responseReceivedHandler() {
+    const weatherInfo = document.getElementById("output");
     if (this.status === 200) {
         console.log(this.response);
         const data = this.response;
-        console.log("city is " + data.places[0]["place name"])
+        
         // put data on the page
-        let output = "<p>City: " + data.places[0]["place name"] + "</p>";
-        output = "<p>Location: " + data.places[0]["longitude"] + "</p>";
+        let output = "<p>City: " + data.city.name + " </p>";
         // output += "<p>Current temp: " + data.main.temp + "&deg;F</p>";
+        output += "<p>Current temp: " + data.list[0].main.temp + "&deg;F</p>";
+        output += "<p>Latitude: " + data.city.coord.lat + "</p>";
+        output += "<p>Longitude: " + data.city.coord.lon + "</p>";
         // output += "<p>Description: " + data.weather[0].description + "</p>";
         // output += "<p>Humidity: " + data.main.humidity + "%</p>";
         // display this in the div
         
-        // weatherInfo.innerHTML = output;
+        weatherInfo.innerHTML = output;
         }
     else {
-        weatherInfo.innerHTML = "Weather Data Unavailable";
+        weatherInfo.innerHTML = "City Unavailable";
     }
 }
+

badc0e1e2535e4790807f4aeab684e0e7fdef302
hina latif <hinalatif25@gmail.com>
2021-02-05 21:49:59 -0500

    part 1 and 2 completed

--- weather.js
+++ weather.js
@@ -1,47 +1,51 @@
-var person = prompt("Please enter your zip code");
+let lon = 0.0;
+let lat = 0.0;
+let temp = document.getElementById('weather');
+let loc = document.getElementById('zipCode');
 
-function getWeather() {
-	let temperature = document.getElementById("temperature");
-	let description = document.getElementById("description");
-	let location = document.getElementById("location");
-  
-	let api = "https://api.openweathermap.org/data/2.5/weather";
-	let apiKey = "f146799a557e8ab658304c1b30cc3cfd";
-  
-	location.innerHTML = "Locating...";
-  
-	navigator.geolocation.getCurrentPosition(success, error);
-  
-	function success(position) {
-	  latitude = position.coords.latitude;
-	  longitude = position.coords.longitude;
-  
-	  let url =
-		api +
-		"?lat=" +
-		latitude +
-		"&lon=" +
-		longitude +
-		"&appid=" +
-		apiKey +
-		"&units=imperial";
-  
-	  fetch(url)
-		.then(response => response.json())
-		.then(data => {
-		  console.log(data);
-		  let temp = data.main.temp;
-		  temperature.innerHTML = temp + "° F";
-		  location.innerHTML =
-		data.name;
-		  description.innerHTML = data.weather[0].main;
-		});
+function getZipCode() {
+    const zipCode = prompt("Enter zip code to get the weather updates of your location");
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let locationResponse = JSON.parse(xhr.responseText);
+            lon = locationResponse.places[0].longitude;
+            lat = locationResponse.places[0].latitude;
+            const city = locationResponse.places[0]["place name"];
+            const state = locationResponse.places[0]["state abbreviation"];
+            // console.log(locationResponse);
+            // console.log(lon);
+            // console.log(lat);
+            getWeather(lon, lat);
+          
+            loc.innerHTML = city + ", " + state;
+        
+        }     
 	}
-  
-	function error() {
-	  location.innerHTML = "Unable to retrieve your location";
+    xhr.open('GET', `http://api.zippopotam.us/US/${zipCode}`);
+    xhr.send();
+}
+
+let xhr2 = new XMLHttpRequest();                
+    xhr2.onreadystatechange = function() { 
+        if (xhr2.readyState === 4 && xhr2.status === 200) {
+            let weatherResponse = JSON.parse(xhr2.responseText);
+           
+            min = weatherResponse['dataseries'][0]['temp2m'].min * 9 / 5 + 32;
+            max = weatherResponse['dataseries'][0]['temp2m'].max * 9 / 5 + 32;
+          
+            console.log(weatherResponse);
+         
+            temp.innerHTML = Math.floor(max) + `&degF`.sup();
+
+            
+        } 
+    }
+
+	function getWeather(lon, lat) {
+		xhr2.open('GET', `http://www.7timer.info/bin/api.pl?product=civillight&lon=${lon}&lat=${lat}&output=json`);
+		xhr2.send();
 	}
-  }
-  
-  getWeather();
-  
+
+
+
